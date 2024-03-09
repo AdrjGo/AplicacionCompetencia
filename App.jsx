0edcca995851d2df6adff4293bec6ed0eb36dd04
AdrjGo <gcasdrianj@gmail.com>
2024-03-09 14:24:13 -0400

    NavBar

--- App.jsx
+++ App.jsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, Button} from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.tituloprincipal}>Inicio</Text>
+      <Button title="Agregar" color="black" style={styles.botonAgregar}></Button>
+
       <StatusBar style="ligth" />
     </SafeAreaView>
   );
@@ -16,8 +18,11 @@ const styles = StyleSheet.create({
     backgroundColor: "steelblue",
   },
   tituloprincipal:{
-    padding:10-5,
-    fontSize:20,
-    fontWeight:blur,
-  }
+    padding: 10-5,
+    fontSize: 20,
+  },
+  botonAgregar:{
+    borderRadius: 10,
+    width:"50%",
+  },
 });
---
+++ screens/HomeScreen.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text, Button, View} from 'react-native';
+
+const HomeScreen = () => {
+    return {
+        
+    };
+}
+
+export default HomeScreen
